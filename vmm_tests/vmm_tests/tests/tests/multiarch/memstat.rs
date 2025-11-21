4022bc9a584aa53b9c4ed19f3794e61f27c6ceac
dpaliulis-msft <dpaliulis@microsoft.com>
2025-11-21 15:07:13 -0800

    vmm_tests: updating memory validation baselines and re-enabling tests (#2467)
    To re-enable memory validation checks, baseline values have been updated
    to pass for the memory usage of current underhill builds.
    
    Changes:
    - Updated all memory validation baselines to pass for the latest memory
    usage
    - Enable memory validation checks in the CI/CD pipeline

--- vmm_tests/vmm_tests/tests/tests/multiarch/memstat.rs
+++ vmm_tests/vmm_tests/tests/tests/multiarch/memstat.rs
@@ -461,8 +461,7 @@ async fn memory_validation_release_small<T: PetriVmmBackend>(
         WaitPeriodSec::ShortWait,
         driver,
         "release",
-        // Disabling test for now to investigate higher memory usage on intel GP and intel TDX tests
-        false,
+        true,
     )
     .await
 }
@@ -488,8 +487,7 @@ async fn memory_validation_debug_small<T: PetriVmmBackend>(
         WaitPeriodSec::ShortWait,
         driver,
         "debug",
-        // Disabling test for now to investigate higher memory usage on intel GP and intel TDX tests
-        false,
+        true,
     )
     .await
 }
@@ -511,8 +509,7 @@ async fn memory_validation_release_very_heavy<T: PetriVmmBackend>(
         WaitPeriodSec::LongWait,
         driver,
         "release",
-        // Disabling test for now to investigate higher memory usage on intel GP and intel TDX tests
-        false,
+        true,
     )
     .await
 }
@@ -538,8 +535,7 @@ async fn memory_validation_debug_very_heavy<T: PetriVmmBackend>(
         WaitPeriodSec::LongWait,
         driver,
         "debug",
-        // Disabling test for now to investigate higher memory usage on intel GP and intel TDX tests
-        false,
+        true,
     )
     .await
 }
