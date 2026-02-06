250fed5cbe2e87f615c0d49590c71cf74a9cee64
Rajesh Gali <g.rajesh@live.com>
2026-02-06 15:29:04 -0600

    openhcl/cvm: allow azi hsm devices  (#2726)
    Work item : msft#59674368
    
    Added Azi HSM device into VPCI filtering list to allow UEFI boot with
    Azi HSM device.
    
    ---------
    
    Co-authored-by: Rajesh Gali <rajeshgali@microsoft.com>

--- openhcl/underhill_core/src/worker.rs
+++ openhcl/underhill_core/src/worker.rs
@@ -198,6 +198,11 @@ pub const UNDERHILL_WORKER: WorkerId<UnderhillWorkerParameters> = WorkerId::new(
 
 const MAX_SUBCHANNELS_PER_VNIC: u16 = 32;
 
+// TODO: Move to hsm crate in future.
+// AZIHSM VPCI IDs
+const AZIHSM_VPCI_VENDOR_ID: u16 = 0x1414;
+const AZIHSM_VPCI_DEVICE_ID: u16 = 0xC003;
+
 struct GuestEmulationTransportInfra {
     get_thread: JoinHandle<()>,
     get_spawner: DefaultDriver,
@@ -3238,6 +3243,18 @@ async fn new_underhill_vm(
                     prog_if: Some(ProgrammingInterface::NETWORK_CONTROLLER_ETHERNET_GDMA),
                     sub_class: Some(Subclass::NETWORK_CONTROLLER_ETHERNET),
                     base_class: Some(ClassCode::NETWORK_CONTROLLER),
+                    sub_vendor_id: None,
+                    sub_system_id: None,
+                });
+
+                // Allow Azi HSM devices.
+                relay.add_allowed_device(AllowedDevice {
+                    vendor_id: Some(AZIHSM_VPCI_VENDOR_ID), // Microsoft vendor ID
+                    device_id: Some(AZIHSM_VPCI_DEVICE_ID), // Azi HSM device ID
+                    revision_id: None,
+                    prog_if: Some(ProgrammingInterface::NONE),
+                    sub_class: Some(Subclass::NONE),
+                    base_class: Some(ClassCode::ENCRYPTION_CONTROLLER),
                     sub_vendor_id: None,
                     sub_system_id: None,
                 });
