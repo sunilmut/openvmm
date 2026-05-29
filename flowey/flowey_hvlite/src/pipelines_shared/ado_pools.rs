b0eb32ad6a6feb6e7f563edbdc2972f7c9e85250
Hadi Orabi <hadiorabi@microsoft.com>
2026-05-28 18:21:22 -0700

    flowey: add an ado intel tdx pool (#3590)
    adds an ado pool for NVMe and TDX testing

--- flowey/flowey_hvlite/src/pipelines_shared/ado_pools.rs
+++ flowey/flowey_hvlite/src/pipelines_shared/ado_pools.rs
@@ -10,11 +10,19 @@ use super::gh_pools::WINDOWS_IMAGE_AMD64;
 
 pub const AMD_POOL_1ES: &str = "openvmm-ado-amd-westus2";
 pub const INTEL_POOL_1ES: &str = "openvmm-ado-intel-centralus";
+pub const INTEL_TDX_POOL: &str = "openvmm-ado-intel-tdx";
 
 fn ado_pool_with_image_1es(pool: &str, image: &str) -> AdoPool {
     AdoPool {
         name: pool.into(),
         demands: vec![format!("ImageOverride -equals {image}")],
+    }
+}
+
+pub fn windows_intel_tdx() -> AdoPool {
+    AdoPool {
+        name: INTEL_TDX_POOL.into(),
+        demands: vec![],
     }
 }
 
